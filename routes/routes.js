6d6fe60cb20880c3f0380d479f824e337ec0666f
Wade Wegner <wade.wegner@gmail.com>
2017-03-04 20:25:45 -0800

    added schema

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,20 @@ var appRouter = function(app) {
         res.send(JSON.stringify({ a: 1 }, null, 3));
     });
 
+    app.get("/schema", function(req, res) {
+
+        var fs = require("fs");
+        const path = require('path');
+
+        var jsonPath = path.join(__dirname, '', 'schema.json')
+        console.log(jsonPath);
+        var content = fs.readFileSync(jsonPath);
+
+        var jsonContent = JSON.parse(content);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(jsonContent, null, 3);
+
+    })
 }
  
 module.exports = appRouter;
