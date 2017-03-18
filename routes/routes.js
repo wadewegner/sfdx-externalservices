80b86c35dc57270846a2ac021ae03e7a7add0e8d
Wade Wegner <wade.wegner@gmail.com>
2017-03-18 08:07:48 -0700

    Fixing issue

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 var appRouter = function(app) {
     app.get("/", function(req, res) {
 
-        res.setHeader('Content-Type', 'application/json');
-        // res.send(JSON.stringify({ a: 1 }, null, 3)); // no complex
+        // res.setHeader('Content-Type', 'application/json');
+        // res.send(JSON.stringify({ a: 1 }, null, 3));
         res.send(1);
     });
 
